4de207bb68aaa6d414298fe587f2ccef484c4cd2
Tom riddle <darklir@gmail.com>
2015-07-30 21:20:46 +0300

    tom2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730121837) do
+ActiveRecord::Schema.define(version: 20150730132835) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,5 +20,15 @@ ActiveRecord::Schema.define(version: 20150730121837) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
